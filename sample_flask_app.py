695fc67a43062da8264e9267e16877b447a15fb9
bjin02 <beijin@yahoo.com>
2017-11-29 19:15:22 -0500

    Final Version

--- sample_flask_app.py
+++ sample_flask_app.py
@@ -1,7 +1,9 @@
 # Import statements necessary
 from flask import Flask, render_template
 from flask_script import Manager
-
+from config import FLICKR_API_KEY
+import requests
+import json
 # Set up application
 app = Flask(__name__)
 
@@ -30,14 +32,16 @@ def basic_values_list(name):
 
 
 ## PART 1: Add another route /word/<new_word> as the instructions describe.
-
+@app.route('/word/<new_word>')
+def new_word(new_word):
+    return '<h1> {}</h1>'.format(new_word)
 
 ## PART 2: Edit the following route so that the photo_tags.html template will render
 @app.route('/flickrphotos/<tag>/<num>')
 def photo_titles(tag, num):
     # HINT: Trying out the flickr accessing code in another file and seeing what data you get will help debug what you need to add and send to the template!
     # HINT 2: This is almost all the same kind of nested data investigation you've done before!
-    FLICKR_KEY = "" # TODO: fill in a flickr key
+    FLICKR_KEY = FLICKR_API_KEY # TODO: fill in a flickr key
     baseurl = 'https://api.flickr.com/services/rest/'
     params = {}
     params['api_key'] = FLICKR_KEY
@@ -49,12 +53,10 @@ def photo_titles(tag, num):
     response_obj = requests.get(baseurl, params=params)
     trimmed_text = response_obj.text[14:-1]
     flickr_data = json.loads(trimmed_text)
-    # TODO: Add some code here that processes flickr_data in some way to get what you nested
-    # TODO: Edit the invocation to render_template to send the data you need
-    return render_template('photo_tags.html')
-
-
-
+    photo_titles = []
+    for each in flickr_data['photos']['photo']:
+        photo_titles.append(each['title'])
+    return render_template('photo_info.html', num = num, photo_titles = photo_titles)
 
 if __name__ == '__main__':
     manager.run() # Runs the flask server in a special way that makes it nice to debug
